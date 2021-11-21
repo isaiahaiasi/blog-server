8d42b925a0e68cb0913e69386301f18ecea5adc3
Isaiah <computerblender@gmail.com>
2021-11-20 19:03:51 -0700

    Refactor docker-compose file to use env PORT
    Also removes default port in "secrets" file, since the Docker container
    doesn't share the default.

--- Makefile
+++ Makefile
@@ -4,4 +4,3 @@ up-prod:
 	docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
 down:
 	docker-compose down
-
--- src/utils/secrets.ts
+++ src/utils/secrets.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 // TODO: handle .env/individual env vars not existing
 dotenv.config({ path: ".env" });
 
-export const PORT = process.env.PORT ?? 3000;
+export const PORT = process.env.PORT;
 export const NODE_ENV = process.env.NODE_ENV;
 export const MONGODB_URI = process.env.MONGODB_URI || "ERROR"; // ! TEMP
 export const JWT_SECRET = process.env.JWT_SECRET || "ERROR";
